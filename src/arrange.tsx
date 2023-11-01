2e977d1719c69702d002bc4ecaa373e3cebf3d50
Aaron Pettengill <pettengill.aaron@gmail.com>
2023-11-01 15:58:40 -0400

    feat: fix up rearranging command

--- src/arrange.tsx
+++ src/arrange.tsx
@@ -2,6 +2,8 @@ import { launchCommand, LaunchType } from "@raycast/api";
 import { runAppleScript } from "@raycast/utils";
 import { z } from "zod";
 
+const Numeric = z.coerce.number();
+
 export default async function arrange() {
   const count = await runAppleScript(`
     set d to 0
@@ -9,9 +11,7 @@ export default async function arrange() {
       set d to count of desktops
     end tell
     return d
-  `).then((res) => {
-    return z.coerce.number().parse(res);
-  });
+  `).then(Numeric.parse);
 
   const activate = (app: string) =>
     runAppleScript(`
@@ -28,18 +28,36 @@ export default async function arrange() {
       type: LaunchType.UserInitiated,
     });
 
-  const moveWindow = async (app: string, singleMonitorCommands: string[], multiMonitorCommands: string[]) => {
+  const isOnLeftDisplay = async (process: string) => {
+    const [xPos] = await runAppleScript(`
+          tell application "System Events"
+            tell process "${process}"
+              return value of attribute "AXPosition" of window 1
+            end tell
+          end tell
+        `).then((res) => res.split(",").map((num) => Numeric.parse(num)));
+    return xPos < 0;
+  };
+
+  const moveWindow = async (
+    app: string,
+    process: string,
+    singleMonitorCommands: string[],
+    multiMonitorCommands: string[],
+  ) => {
     const numDesktops = await count;
     await activate(app);
     const commands = numDesktops === 1 ? singleMonitorCommands : multiMonitorCommands;
     for (const command of commands) {
+      if (command === "previous-display" && (await isOnLeftDisplay(process))) continue;
+      if (command === "next-display" && !(await isOnLeftDisplay(process))) continue;
       await new Promise((resolve) => setTimeout(resolve, 100));
       await windowCommand(command);
     }
   };
 
-  await moveWindow("Sublime Merge", ["left-half"], ["previous-desktop", "maximize"]);
-  await moveWindow("Warp", ["right-half"], ["first-fourth"]);
-  await moveWindow("Visual Studio Code", ["left-half"], ["center-half"]);
-  await moveWindow("Arc", ["right-half"], ["first-fourth"]);
+  await moveWindow("Sublime Merge", "Sublime Merge", ["left-half"], ["previous-display", "maximize"]);
+  await moveWindow("Warp", "Warp", ["right-half"], ["next-display", "first-fourth"]);
+  await moveWindow("Arc", "Arc", ["right-half"], ["next-display", "last-fourth"]);
+  await moveWindow("Visual Studio Code", "Code", ["left-half"], ["next-display", "center-half"]);
 }
